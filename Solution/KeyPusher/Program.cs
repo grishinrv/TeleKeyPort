8a7ff4f5ca7f4dec13d9a1c7089b21389dc0d609
rgrishin <roman1988grishin@gmail.com>
2021-06-20 21:04:39 +0300

    remove form, add tray

--- Solution/KeyPusher/Program.cs
+++ Solution/KeyPusher/Program.cs
@@ -9,18 +9,41 @@ namespace KeyPusher
 {
     static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            var service = new KeyEventsDetector();
-            service.KeyEventHappened += (o, args) => MessageBox.Show($"{args.Key}");
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new KeyPusherApp());
+        }
+    }
+
+    public class KeyPusherApp : ApplicationContext
+    {
+        private NotifyIcon _trayIcon;
+        private KeyEventsDetector _keysDetector;
+
+        public KeyPusherApp()
+        {
+            _keysDetector = new KeyEventsDetector();
+            _keysDetector.KeyEventHappened += (o, args) => MessageBox.Show($"{args.Key}");
+            _trayIcon = new NotifyIcon()
+            {
+                //Icon = ToolTipIcon.Info,
+                //Me
+                //ContextMenu = new ContextMenu(new MenuItem[] {
+                //    new MenuItem("Exit", Exit)
+                //}),
+                Visible = true
+            };
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+            _keysDetector.Dispose();
+            Application.Exit();
         }
     }
 }
