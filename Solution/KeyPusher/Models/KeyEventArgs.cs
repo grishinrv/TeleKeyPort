0b22ef4a2d73492a37adc4ad4ecba5017df4f6eb
rgrishin <roman1988grishin@gmail.com>
2021-06-20 20:45:17 +0300

    Solution draft + key events

---
+++ Solution/KeyPusher/Models/KeyEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyPusher.Models
+{
+    /// <summary>
+    /// Key live cycle event args.
+    /// </summary>
+    public class KeyEventArgs : EventArgs
+    {
+        public KeyEventArgs(Keys key)
+        {
+            Key = key;
+        }
+
+        public Keys Key { get; }
+    }
+}
