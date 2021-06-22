64105dcd8cab99060b723157637fcfd6d5ad823a
rgrishin <roman1988grishin@gmail.com>
2021-06-22 08:44:39 +0300

    TCP socket dependency + pseudo key pressing demo

---
+++ Solution/KeyReceiverService/Services/KeyBoardProxy.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace KeyReceiverService.Services
+{
+    public class KeyBoardProxy
+    {
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        public void PressKey()
+        {
+            const int VK_UP = 0x26; //up key
+            const int VK_DOWN = 0x28; //down key
+            const int VK_LEFT = 0x25;
+            const int VK_RIGHT = 0x27;
+            const uint KEYEVENTF_KEYUP = 0x0002;
+            const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(500);
+                    press();
+                }
+
+
+                int press()
+                {
+                    //Press the key
+                    keybd_event((byte) 0x45, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+                    return 0;
+                }
+
+                int release()
+                {
+                    //Release the key
+                    keybd_event((byte) VK_RIGHT, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                    return 0;
+                }
+            });
+        }
+    }
+}
--- Solution/KeyReceiverService/Worker.cs
+++ Solution/KeyReceiverService/Worker.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using KeyReceiverService.Services;
 
 namespace KeyReceiverService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly KeyBoardProxy _keyBoard;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _keyBoard = new KeyBoardProxy();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,6 +25,7 @@ namespace KeyReceiverService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _keyBoard.PressKey();
                 await Task.Delay(1000, stoppingToken);
             }
         }
